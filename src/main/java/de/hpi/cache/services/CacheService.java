f88003ba06f65f69ccde641e591cbad1d4df29c8
Daniel Lindner <lindnerdaniel@yahoo.de>
2018-06-04 15:12:03 +0200

    add javadoc

--- src/main/java/de/hpi/cache/services/CacheService.java
+++ src/main/java/de/hpi/cache/services/CacheService.java
@@ -24,14 +24,35 @@ public class CacheService {
     private static final Logger logger = LogManager.getLogger(CacheService.class);
 
 
+    /**
+     *
+     * @param shopId The ID of the shop
+     * @param offerKey The ID of the offer
+     * @return The offer specified by shopId and offerKey
+     */
     public ShopOffer getOffer(long shopId, String offerKey) {
         return getRepository().getOffer(shopId, offerKey);
     }
 
+    /**
+     * This method finds the first offer for the specified shop with the requested phase and the matched flag set to false.
+     * It also increases the phase of the offer.
+     * @param shopId The ID of the shop
+     * @param phase The matching phase the offer should be in
+     * @return The first offer of the shop in the requested phase that was not already matched
+     */
     public ShopOffer getUnmatchedOfferAndUpdatePhase(long shopId, byte phase) {
         return getRepository().getUnmatchedOfferAndUpdatePhase(shopId, phase);
     }
 
+    /**
+     * This method finds the first offer for the specified shop with the requested phase.
+     * It also increases the phase of the offer.
+     * @param shopId The ID of the shop
+     * @param phase The matching phase the offer should be in
+     * @param currentlyWarmingUp Mutex to avoid multiple warming up of same shop. Used to make sure requested shop is already downloaded.
+     * @return The first offer of the shop in the requested phase
+     */
     public ShopOffer getOfferAndUpdatePhase(long shopId, byte phase, WarmingUpShops currentlyWarmingUp) {
         while(currentlyWarmingUp.isWarmingUp(shopId)) {
             try {
@@ -43,18 +64,38 @@ public class CacheService {
         return getRepository().getOfferAndUpdatePhase(shopId, phase);
     }
 
+
     public void markAsMatched(long shopId, String offerKey) {
         getRepository().markAsMatched(shopId, offerKey);
     }
 
+    /**
+     * This method sets the phase of all offers of the requested shop with specified phase to the new one.
+     * @param shopId The ID of the shop
+     * @param oldPhase The current phase
+     * @param newPhase The new phase
+     */
     public void updatePhase(long shopId, byte oldPhase, byte newPhase) {
         getRepository().updatePhase(shopId, oldPhase, newPhase);
     }
 
+    /**
+     * This method deletes all offers of one shop.
+     * @param shopId The ID of the shop
+     */
     public void deleteAll(long shopId) {
         getRepository().deleteAll(shopId);
     }
 
+    /**
+     * This method starts downloading and preparing all offers for one shop.
+     * For every offer, it will<br />
+     * 1. add category information (actual category and higher level category),<br />
+     * 2. clean the URL and <br />
+     * 3. extract unique parts of image URL (image ID).
+     * @param shopId The ID of the shop
+     * @param currentlyWarmingUp Mutex to avoid multiple warming up of same shop.
+     */
     public void warmup(long shopId, WarmingUpShops currentlyWarmingUp){
         if(currentlyWarmingUp.isWarmingUp(shopId)) {
             return;
